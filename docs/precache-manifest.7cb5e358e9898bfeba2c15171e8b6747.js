b2d71bb22d7322ce34f81335a876b7185ccd2603
X3R02 <x3r02mail@gmail.com>
2020-10-25 11:28:35 -0600

    Docs added

---
+++ docs/precache-manifest.7cb5e358e9898bfeba2c15171e8b6747.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4910aae6b4c2e604a4236774fcc1c36a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20fe15c8125b18026c2e",
+    "url": "/static/css/main.60eb00d8.chunk.css"
+  },
+  {
+    "revision": "27de45221db231b10e67",
+    "url": "/static/js/2.bc29d3ad.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.bc29d3ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20fe15c8125b18026c2e",
+    "url": "/static/js/main.b4e79b16.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
